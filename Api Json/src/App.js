b20136e3acdc9e082c53b2b21d015e97c767f8fa
lucasbarrost <lucasbarrost9@gmail.com>
2024-08-08 14:01:49 -0300

    Livro

---
+++ Api Json/src/App.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import axios from 'axios'
+import PostList from "./components/PostList";
+import PostDetail from "./components/PostDetail";
+
+function App() {
+  const [posts, setPosts]= useState([])
+  const [selectedPost, setSelectedPost] = useState(null)
+  
+  useEffect(()=>{
+    const fetchPosts = async () =>{
+      try{
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      setPosts(res.data)
+    } catch(e){
+      console.log(e, 'erro ao renderizar posts')
+
+      }
+    }
+    fetchPosts()
+  },[])
+
+  const handleSelectPost = (post) =>{
+    setSelectedPost(post)
+  }
+
+  return (
+    <div>
+      <PostList posts={posts} onSelectPost={handleSelectPost}/>
+      <PostDetail post={selectedPost}/>
+    </div>
+  
+  );
+}
+
+export default App;
