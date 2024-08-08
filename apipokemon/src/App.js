3648dd3b133632197f9dfb4602bb863dbc4fed82
lucasbarrost <lucasbarrost9@gmail.com>
2024-08-08 15:19:46 -0300

    Pokemon

---
+++ apipokemon/src/App.js
@@ -0,0 +1,15 @@
+// Importando módulos necessários
+import React from 'react';
+import './Styles/App.css'; // Importando estilos globais
+import PokemonList from './components/PokemonList';// Importando o componente PokemonList
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokémon App</h1>
+      <PokemonList />
+    </div>
+  );
+}
+
+export default App;
